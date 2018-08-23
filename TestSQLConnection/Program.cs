2d60d6591b31e6bde21b1d60f060dd4492e80c7e
Russ Johnson <russ@johnsonville.net>
2018-08-23 13:52:57 -0500

    Initial commit

---
+++ TestSQLConnection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace TestSQLConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "Server=" + args[0] + ";Initial Catalog=master;Integrated Security=sspi;Persist Security Info=true";
+            Console.WriteLine("Connecting to " + conString);
+            using (var connection = new SqlConnection(conString))
+            {
+                var command = new SqlCommand("SELECT CURRENT_TIMESTAMP", connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]}");
+                    }
+                }
+            }
+        }
+    }
+}
